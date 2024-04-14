fbec7bc244942bab3df3969e7c3cb5a20e091a82
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-04-14 19:05:13 -0300

    feito os campos de cadastro do hotel

--- lib/model-view/create-hotel-vm.dart
+++ lib/model-view/create-hotel-vm.dart
@@ -4,5 +4,5 @@ import 'package:image_picker/image_picker.dart';
 class CreateHotelViewModel{
 
   List<XFile> filesImage = [];
-
+  String sizeType = 'SMALL';
 }
--- lib/views/enterprise/page-create-hotel-information.dart
+++ lib/views/enterprise/page-create-hotel-information.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:apphotelbooking/model-view/create-hotel-vm.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:masked_text/masked_text.dart';
 
 import '../../model-view/register-vm.dart';
 import '../shared-view-widgets/colors.dart';
@@ -14,22 +19,161 @@ class PageCreateHotelInformation extends StatefulWidget {
 
   @override
   State<PageCreateHotelInformation> createState() =>
-      _PageCreateHotelInformationState();
+      _PageCreateHotelInformationState(_createHotelViewModel);
 }
 
 class _PageCreateHotelInformationState
     extends State<PageCreateHotelInformation> {
+  _PageCreateHotelInformationState(this._createHotelViewModel);
+
+  final CreateHotelViewModel _createHotelViewModel;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.90,
-      child: Column(
-        children: [
-          WidgetsDecorated.textDecorated(
-              20,
-              'Olá ${widget._registerMV.name} para começar a oferecer reservas registre seu Hotel!',
-              ColorsView.black),
-        ],
+      height: MediaQuery.of(context).size.height * 1,
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                WidgetsDecorated.textDecorated(
+                    20,
+                    'Olá ${widget._registerMV.name} para começar a oferecer reservas, registre seu Hotel!',
+                    ColorsView.black),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatorio';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {},
+                  style: const TextStyle(
+                    fontFamily: 'principal',
+                    fontSize: 17,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: 'Nome do hotel',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                WidgetsDecorated.textDecorated(
+                    16, 'Selecione o porte do hotel', ColorsView.black),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: _createHotelViewModel.sizeType,
+                  items: const [
+                    DropdownMenuItem(
+                      value: 'LARGE',
+                      child: Text('Grande'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'MEDIUM',
+                      child: Text('Medio'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'SMALL',
+                      child: Text('Pequeno'),
+                    ),
+                  ],
+                  onChanged: (value) {
+                    _createHotelViewModel.sizeType = value!;
+                    setState(() {});
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatorio';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {},
+                  style: const TextStyle(
+                    fontFamily: 'principal',
+                    fontSize: 17,
+                  ),
+                  decoration: const InputDecoration(
+                      labelText: 'Rua', counterText: 'ex: Rua jose bonifacio'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.36,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Campo obrigatorio';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {},
+                        maxLength: 7,
+                        keyboardType: TextInputType.number,
+                        style: const TextStyle(
+                          fontFamily: 'principal',
+                          fontSize: 17,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Numero da rua',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.38,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Campo obrigatorio';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {},
+                        maxLength: 20,
+                        style: const TextStyle(
+                          fontFamily: 'principal',
+                          fontSize: 17,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Bairro',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Campo obrigatorio';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {},
+                  style: const TextStyle(
+                    fontFamily: 'principal',
+                    fontSize: 17,
+                  ),
+                  decoration: const InputDecoration(
+                      labelText: 'Cidade', counterText: 'ex: Campinas'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
